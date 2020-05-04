1b4507267aa217a61395855100ab7085e9d88247
Lincoln Arrais <lincoln_arrais@hotmail.com>
2020-05-04 00:17:11 -0300

    Implementei um pouco o "sort". Fiz uma otimização praticamente insignificante ao remover uma tarefa.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
-import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main(){
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+void main() {
+  runApp(
+    MaterialApp(
+      home: Home(),
+      theme: ThemeData(
+        primaryColor: Colors.blueAccent,
+      ),
+    ),
+  );
 }
 
 class Home extends StatefulWidget {
@@ -17,11 +21,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-  final _toDoController = TextEditingController();
-
   List _toDoList = [];
 
+  final _toDoController = TextEditingController();
   Map<String, dynamic> _lastRemoved;
   int _lastRemovedPos;
 
@@ -39,24 +41,24 @@ class _HomeState extends State<Home> {
   void _addToDo() {
     setState(() {
       Map<String, dynamic> newToDo = Map();
-      newToDo["title"] = _toDoController.text;
-      _toDoController.text = "";
-      newToDo["ok"] = false;
+      newToDo['title'] = _toDoController.text;
+      _toDoController.text = '';
+      newToDo['ok'] = false;
+      newToDo['dateTime'] = DateTime.now().toIso8601String();
       _toDoList.add(newToDo);
+
+      print('title: ${newToDo['title']}\ndateTime: ${newToDo['dateTime']}');
 
       _saveData();
     });
   }
 
-  Future<Null> _refresh() async{
+  Future<Null> _refresh() async {
     await Future.delayed(Duration(seconds: 1));
 
     setState(() {
-      _toDoList.sort((a, b){
-        if(a["ok"] && !b["ok"]) return 1;
-        else if(!a["ok"] && b["ok"]) return -1;
-        else return 0;
-      });
+      _sortByCreationDate();
+      _sortByUnchecked();
 
       _saveData();
     });
@@ -64,97 +66,127 @@ class _HomeState extends State<Home> {
     return null;
   }
 
+  void _sortByUnchecked() {
+    _toDoList.sort((a, b) {
+      if (a['ok'] && !b['ok'])
+        return 1;
+      else if (!a['ok'] && b['ok'])
+        return -1;
+      else
+        return 0;
+    });
+  }
+
+  void _sortByCreationDate() {
+    _toDoList.sort((a, b) {
+      final Duration difference = DateTime.parse(a['dateTime'])
+          .difference(DateTime.parse(b['dateTime']));
+
+      return difference.inSeconds != 0
+          ? difference.inSeconds
+          : difference.inMilliseconds;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Lista de Tarefas"),
-        backgroundColor: Colors.blueAccent,
-        centerTitle: true,
+        title: Text('Lista de Tarefas'),
+        backgroundColor: Theme.of(context).primaryColor,
       ),
       body: Column(
         children: <Widget>[
           Container(
-            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            padding:
+                EdgeInsets.only(left: 17.0, top: 1.0, right: 7.0, bottom: 1.0),
             child: Row(
               children: <Widget>[
                 Expanded(
                   child: TextField(
                     controller: _toDoController,
                     decoration: InputDecoration(
-                        labelText: "Nova Tarefa",
-                        labelStyle: TextStyle(color: Colors.blueAccent)
+                      labelText: 'Nova Tarefa',
+                      labelStyle:
+                          TextStyle(color: Theme.of(context).primaryColor),
                     ),
-                  )
+                  ),
                 ),
                 RaisedButton(
-                  color: Colors.blueAccent,
-                  child: Text("ADD"),
+                  color: Theme.of(context).primaryColor,
+                  child: Text('ADD'),
                   textColor: Colors.white,
                   onPressed: _addToDo,
-                )
+                ),
               ],
             ),
           ),
           Expanded(
-            child: RefreshIndicator(onRefresh: _refresh,
+            child: RefreshIndicator(
               child: ListView.builder(
-                  padding: EdgeInsets.only(top: 10.0),
-                  itemCount: _toDoList.length,
-                  itemBuilder: buildItem),),
-          )
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: buildItem,
+              ),
+              onRefresh: _refresh,
+            ),
+          ),
         ],
       ),
     );
   }
 
-  Widget buildItem(BuildContext context, int index){
+  Widget buildItem(BuildContext context, int index) {
     return Dismissible(
-      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
       background: Container(
         color: Colors.red,
         child: Align(
           alignment: Alignment(-0.9, 0.0),
-          child: Icon(Icons.delete, color: Colors.white,),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
         ),
       ),
       direction: DismissDirection.startToEnd,
+      key: Key(Random().nextDouble().toString()),
       child: CheckboxListTile(
-        title: Text(_toDoList[index]["title"]),
-        value: _toDoList[index]["ok"],
+        title: Text(_toDoList[index]['title']),
+        value: _toDoList[index]['ok'],
         secondary: CircleAvatar(
-          child: Icon(_toDoList[index]["ok"] ?
-          Icons.check : Icons.error),),
-        onChanged: (c){
+          child: Icon(
+            _toDoList[index]['ok'] ? Icons.check : Icons.error,
+          ),
+        ),
+        onChanged: (c) {
           setState(() {
-            _toDoList[index]["ok"] = c;
+            _toDoList[index]['ok'] = c;
             _saveData();
           });
         },
       ),
-      onDismissed: (direction){
+      onDismissed: (_) {
         setState(() {
-          _lastRemoved = Map.from(_toDoList[index]);
           _lastRemovedPos = index;
-          _toDoList.removeAt(index);
+          _lastRemoved = _toDoList.removeAt(index);
 
           _saveData();
 
           final snack = SnackBar(
-            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
-            action: SnackBarAction(label: "Desfazer",
-                onPressed: () {
-                  setState(() {
-                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
-                    _saveData();
-                  });
-                }),
-            duration: Duration(seconds: 2),
+            content: Text('Tarefa "${_lastRemoved['title']}" removida!'),
+            action: SnackBarAction(
+              label: 'Desfazer',
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                });
+              },
+            ),
+            duration: Duration(seconds: 4),
           );
 
           Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
-
         });
       },
     );
@@ -162,7 +194,7 @@ class _HomeState extends State<Home> {
 
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
-    return File("${directory.path}/data.json");
+    return File('${directory.path}/data.json');
   }
 
   Future<File> _saveData() async {
@@ -175,12 +207,9 @@ class _HomeState extends State<Home> {
   Future<String> _readData() async {
     try {
       final file = await _getFile();
-
       return file.readAsString();
     } catch (e) {
       return null;
     }
   }
-
 }
-
